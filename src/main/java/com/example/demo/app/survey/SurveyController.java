922f45751dee3d2be2711b258e94b226016c01de
filament44 <concier11@gmail.com>
2020-06-05 18:10:04 +0900

    surveyも完成

--- src/main/java/com/example/demo/app/survey/SurveyController.java
+++ src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,5 +1,8 @@
 package com.example.demo.app.survey;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Survey;
 import com.example.demo.service.SurveyService;
 
 @Controller
@@ -27,7 +31,10 @@ public class SurveyController {
 	@GetMapping
 	public String index(Model model) {
 		
-		//hands-on
+		List<Survey> list = surveyService.getAll();
+		
+		model.addAttribute("SurveyList", list);
+		model.addAttribute("title", "Survey Index");
 		
 		return "survey/index";
 	}
@@ -73,6 +80,14 @@ public class SurveyController {
 			return "survey/form";
 		
 		}
+		
+		Survey survey = new Survey();
+		survey.setAge(surveyForm.getAge());
+		survey.setSatisfaction(surveyForm.getSatisfaction());
+		survey.setComment(surveyForm.getComment());
+		survey.setCreated(LocalDateTime.now());
+		surveyService.save(survey);
+		
 		redirectAttributes.addFlashAttribute("complete", "Thank you");
 		return "redirect:/survey/form";
 	}
